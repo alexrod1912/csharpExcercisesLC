e006bd9027e335b01e953ad739fec8a5dfa2ed22
Naomi Rodriguez <naomirodriguez@Naomis-MacBook-Air.local>
2024-06-02 21:46:37 -0500

    finished the exercises for the exceptions chapter

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,30 +1,58 @@
 ﻿// --------- Divide By Zero! ----------
 
-/* 
+
 static double Divide(double x, double y)
 {
-    // Write your code here!
+     if (y == 0.0)
+      {
+         throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!");
+      }
+      else
+      {
+         Console.WriteLine(x/y);
+         return x / y;
+      }
 }
-*/
+
 
 // Test out your Divide() function here!
-
+    // Divide(7, 1);
 
 // --------- Test Student Labs ----------
 
-/*
+
 static int CheckFileExtension(string fileName)
 {
-    // Write your code here!
+    if(fileName == null || fileName == ""){
+        throw new ArgumentNullException("filename", "Filename cannot be null");
+    }
+    else {
+        if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+         {
+            Console.WriteLine("1");
+            return 1;
+         }
+         else
+         {
+            Console.WriteLine("0");
+            return 0;
+         }
+    }
 }
-*/
+
 
 // Test out your CheckFileExtension() function here!
 
+
+
 Dictionary<string, string> students = new Dictionary<string, string>();
 students.Add("Carl", "Program.cs");
+students.Add("Elizabeth", "MyCode");
 students.Add("Brad", "");
-students.Add("Elizabeth", "MyCode.cs");
 
+foreach (var student in students)
+{
+    CheckFileExtension(student.Value);
+}
 
 
